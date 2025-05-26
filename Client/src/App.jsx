38430b4a9ec0c19e3ec6418149fc418c2426dc21
aditya <asadityasingh2572000@gmail.com>
2025-05-26 20:08:01 +0530

    Complete Routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 const App = () => {
-  return <div>Appghjklkj</div>
+  return (
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
 }
 
 export default App
