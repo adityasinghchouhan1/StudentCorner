e8c94163c778bfa6ea579c8801a002389dad00a9
aditya <asadityasingh2572000@gmail.com>
2025-05-29 22:08:23 +0530

    start creating nevbar

---
+++ Client/src/Component/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="flex justify-between items-center p-2">
+        <div>
+          <img src="" alt="LOgo" />
+        </div>
+
+        <div></div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
